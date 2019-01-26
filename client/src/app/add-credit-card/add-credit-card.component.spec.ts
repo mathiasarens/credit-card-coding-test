3de15cf42719bc8f1827c96f82bbd395c6a7b382
Mathias Arens <mathias.arens@googlemail.com>
2019-01-26 23:06:09 +0000

    Added Router, Initial Overview component

--- client/src/app/add-credit-card/add-credit-card.component.spec.ts
+++ client/src/app/add-credit-card/add-credit-card.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AddCreditCardComponent } from './addcreditcard.component';
+import { AddCreditCardComponent } from './add-credit-card.component';
 
 describe('AddcreditcardComponent', () => {
   let component: AddCreditCardComponent;
